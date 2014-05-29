0b1b9a3d41657ac904c3744fcd41ace28b73ea55
SharerMax <mdcw1103@gmail.com>
2014-05-29 11:50:08 +0800

    remember layout, translate layout

--- MWord/src/net/sharermax/mword/MainActivity.java
+++ MWord/src/net/sharermax/mword/MainActivity.java
@@ -1,15 +1,28 @@
 package net.sharermax.mword;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+	private RememberFragment rememberFragment;
+	private TranslateFragment searchFragment;
+	private FragmentManager fragmentManager;
+	private boolean currentFragment;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		rememberFragment = new RememberFragment();
+		searchFragment = new TranslateFragment();
+		
+		fragmentManager = getFragmentManager();
+		fragmentManager.beginTransaction().replace(R.id.content, rememberFragment).commit();
+		currentFragment = true;
 	}
 
 	@Override
@@ -19,4 +32,28 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.action_switching:
+			if(currentFragment) {
+				fragmentManager.beginTransaction().replace(R.id.content, searchFragment).commit();
+				
+			}else {
+				fragmentManager.beginTransaction().replace(R.id.content, rememberFragment).commit();
+			}
+			currentFragment = !currentFragment;
+			return true;
+		case R.id.action_settings:
+			Intent intent = new Intent();
+			intent.setClass(this, SettingActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
 }
---
+++ MWord/src/net/sharermax/mword/SettingActivity.java
@@ -0,0 +1,16 @@
+package net.sharermax.mword;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setting_layout);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+}
