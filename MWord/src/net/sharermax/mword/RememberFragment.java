ebfb20211638f9e301a30bacb74b15f18dc84c24
SharerMax <mdcw1103@gmail.com>
2014-06-03 00:10:21 +0800

    modify remember description layout

--- MWord/src/net/sharermax/mword/RememberFragment.java
+++ MWord/src/net/sharermax/mword/RememberFragment.java
@@ -1,6 +1,5 @@
 package net.sharermax.mword;
 
-import android.R.integer;
 import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -16,6 +15,7 @@ public class RememberFragment extends Fragment {
 	private final static String REM_FONT_SIZE_KEY = "rem_font_size_key";
 	private final static String REM_FONT_COLOR_KEY = "rem_font_color_key";
 	private TextView rem_word_show;
+	private TextView rew_des_show;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -29,8 +29,15 @@ public class RememberFragment extends Fragment {
 		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
 		int remFontColor = sharedPreferences.getInt(REM_FONT_COLOR_KEY, 0xff000000);
 		int remFontSize = sharedPreferences.getInt(REM_FONT_SIZE_KEY, 2);
-		rem_word_show = (TextView)(getView().findViewById(R.id.rem_word_show));
 		rem_word_show.setTextColor(remFontColor);
 		rem_word_show.setTextSize(TypedValue.COMPLEX_UNIT_SP, (remFontSize +  1) * 10);
 	}
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		rem_word_show = (TextView)(getView().findViewById(R.id.rem_word_show));
+		rew_des_show = (TextView)(getView().findViewById(R.id.rem_des_show));
+	}
+	
 }
