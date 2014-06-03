1517b4785aa14791c162c4972572014e398b7680
SharerMax <mdcw1103@gmail.com>
2014-06-04 00:56:28 +0800

    add GestureDetector base interface

--- MWord/src/net/sharermax/mword/RememberFragment.java
+++ MWord/src/net/sharermax/mword/RememberFragment.java
@@ -4,10 +4,18 @@ import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RememberFragment extends Fragment {
@@ -16,6 +24,8 @@ public class RememberFragment extends Fragment {
 	private final static String REM_FONT_COLOR_KEY = "rem_font_color_key";
 	private TextView rem_word_show;
 	private TextView rew_des_show;
+	private ImageView rem_query_image;
+	private GestureDetector gestureDetector;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -38,6 +48,73 @@ public class RememberFragment extends Fragment {
 		super.onActivityCreated(savedInstanceState);
 		rem_word_show = (TextView)(getView().findViewById(R.id.rem_word_show));
 		rew_des_show = (TextView)(getView().findViewById(R.id.rem_des_show));
+		rem_query_image = (ImageView)(getView().findViewById(R.id.rem_query_image));
+		rew_des_show.setLongClickable(true);
+		
+		MyGestureDetector myGestureDetector = new MyGestureDetector();
+		gestureDetector = new GestureDetector(getActivity(),myGestureDetector);
+		
+		rew_des_show.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View arg0, MotionEvent event) {
+				// TODO Auto-generated method stub
+				return gestureDetector.onTouchEvent(event);
+			}
+		});
+		
+		rem_query_image.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Log.v("imageview", "OK");
+				rem_query_image.setEnabled(false);
+				rem_query_image.setVisibility(View.GONE);
+			}
+		});
+		
+	}
+	public class MyGestureDetector  extends SimpleOnGestureListener{
+
+		@Override
+		public boolean onDown(MotionEvent arg0) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean onFling(MotionEvent arg0, MotionEvent arg1, float arg2,
+				float arg3) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void onLongPress(MotionEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2,
+				float arg3) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public void onShowPress(MotionEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public boolean onSingleTapUp(MotionEvent arg0) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+		
 	}
 	
 }
